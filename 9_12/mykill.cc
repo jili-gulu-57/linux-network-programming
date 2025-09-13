f0dde353a23f377df6fcd6d910d0310cf9287b5d
jili-gulu-57 <2263293720@qq.com>
2025-09-13 21:05:45 +0800

    Linux信号（二）

--- 9_12/mykill.cc
+++ 9_12/mykill.cc
@@ -9,40 +9,54 @@ void handler(int signo)
     return;
 }
 
-//  ./mykill 9 12424
-int main(int argc, char *argv[])
+int main()
 {
+    signal(11,handler);
 
-    alarm(3);
-
-    while(true)
-    {
-        std::cout<<"1"<<std::endl;
-        sleep(1);
-    }
-
-    // if (argc != 3)
-    // {
-    //     std::cout << "Usage:" << argv[0] << "signumber pid" << std::endl;
-    //     return 1;
-    // }
-    // int signumber = std::stoi(argv[1]);
-    // pid_t target = std::stoi(argv[2]);
-
-    // int n = kill(target, signumber);
-    // if (n < 0)
-    // {
-    //     perror("kill");
-    //     return 2;
-    // }
-
-    // signal(6,handler);
-
-    // while(true)
-    // {
-    //     abort();   //程序自己给自己发信号
-    //     sleep(1);
-    // }
-
+    // int a=10;
+    // a/=0;
+    int *p=nullptr;
+    *p=10;
+    signal(8,handler);
+    printf("进程崩溃了\n");
     return 0;
 }
+
+
+//  ./mykill 9 12424
+// int main(int argc, char *argv[])
+// {
+
+//     alarm(3);
+
+//     while(true)
+//     {
+//         std::cout<<"1"<<std::endl;
+//         sleep(1);
+//     }
+
+//     // if (argc != 3)
+//     // {
+//     //     std::cout << "Usage:" << argv[0] << "signumber pid" << std::endl;
+//     //     return 1;
+//     // }
+//     // int signumber = std::stoi(argv[1]);
+//     // pid_t target = std::stoi(argv[2]);
+
+//     // int n = kill(target, signumber);
+//     // if (n < 0)
+//     // {
+//     //     perror("kill");
+//     //     return 2;
+//     // }
+
+//     // signal(6,handler);
+
+//     // while(true)
+//     // {
+//     //     abort();   //程序自己给自己发信号
+//     //     sleep(1);
+//     // }
+
+//     return 0;
+// }
