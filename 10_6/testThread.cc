83bcd66fd6533f565b5cd0a2380f6018e928e68f
jili-gulu-57 <2263293720@qq.com>
2025-10-07 23:11:07 +0800

    线程控制一（未完成）

--- 10_6/testThread.cc
+++ 10_6/testThread.cc
@@ -1,21 +1,32 @@
-#include<iostream>
-#include<unistd.h>
-#include<pthread.h>
+#include <iostream>
+#include <unistd.h>
+#include <pthread.h>
 
-void *thread_routine(void * name)
+volatile int gval = 100;
+
+void *thread_routine(void *args)
 {
-    std::cout<<"new thread"<<std::endl;
-    sleep(1);
+    std::string threadname = static_cast<const char *>(args);
+    while (true)
+    {
+        std::cout << "new thread";
+        printf("gval:%d,&gval:%p\n", gval, &gval);
+        gval++;
+        sleep(1);
+    }
+    
 }
 
 int main()
 {
     pthread_t tid;
-    pthread_create(&tid,nullptr,thread_routine,(void *)"thread -l");
+    int n = pthread_create(&tid, nullptr, thread_routine, (void *)"thread -l");
+    (void)n;
 
-    while(true)
+    while (true)
     {
-        std::cout<<"main thread"<<std::endl;
+        std::cout << "main thread";
+        printf("gval:%d,&gval:%p\n", gval, &gval);
         sleep(1);
     }
     return 0;
