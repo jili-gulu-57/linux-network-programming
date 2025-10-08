fb1ab8bb45b99b44a137bc2812109a3d5e0a63fb
jili-gulu-57 <2263293720@qq.com>
2025-10-08 22:19:45 +0800

    线程控制（一）

--- 10_6/testThread.cc
+++ 10_6/testThread.cc
@@ -9,12 +9,15 @@ void *thread_routine(void *args)
     std::string threadname = static_cast<const char *>(args);
     while (true)
     {
-        std::cout << "new thread";
+        std::cout << "new thread"<<std::endl;
         printf("gval:%d,&gval:%p\n", gval, &gval);
         gval++;
         sleep(1);
+        // break;
     }
-    
+    // return nullptr;  //第一种退出方法
+    // exit(10);   //exit是用来终止进程，任何一个线程调用则会导致整个进程退出，即所有线程退出  
+    // pthread_exit(nullptr);  //第二种退出方法
 }
 
 int main()
@@ -23,11 +26,19 @@ int main()
     int n = pthread_create(&tid, nullptr, thread_routine, (void *)"thread -l");
     (void)n;
 
-    while (true)
-    {
-        std::cout << "main thread";
-        printf("gval:%d,&gval:%p\n", gval, &gval);
-        sleep(1);
-    }
+    // int m=pthread_join(tid,nullptr);
+    // (void)m;
+    sleep(3);
+    printf("main thread\n");
+
+    //第三种，主线程取消其他线程
+    pthread_cancel(tid);
+
+    // while (true)
+    // {
+    //     std::cout << "main thread";
+    //     printf("gval:%d,&gval:%p\n", gval, &gval);
+    //     sleep(1);
+    // }
     return 0;
 }
