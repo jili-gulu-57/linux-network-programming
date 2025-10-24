1397d58fb873365af568c35738661bdc40b8c89b
jili-gulu-57 <2263293720@qq.com>
2025-10-24 22:40:27 +0800

    线程同步

--- 10_22/BlockQueue/BlockQueue.hpp
+++ 10_22/BlockQueue/BlockQueue.hpp
@@ -11,6 +11,10 @@ class BlockQueue
     {
         return _bq.size()>=_cap;
     }
+    bool IsEmpty()
+    {
+        return _bq.empty();
+    }
 public:
     BlockQueue(u_int32_t cap=gcap):_cap(cap)
     {
@@ -18,24 +22,41 @@ public:
         pthread_cond_init(&_c_cond,nullptr);
         pthread_cond_init(&_p_cond,nullptr);
     }
+
     void Enqueue(const T &in)
     {
         pthread_mutex_lock(&_lock);
-        pthread_mutex_unlock(&_lock);
 
         if(IsFull())
         {
             //队列已满
-
+            //原子性进入等待，调用pthread_cond_wait之前，线程必须已经获得了互斥锁，pthread_cond_wait会做三件事：
+            //1原子地解放mutex 2将当前线程挂起，进入等待cond状态 3收到信号被唤醒后，重新尝试获取mutex锁
+            pthread_cond_wait(&_p_cond,&_lock);
         }
+        //队列不满
+        _bq.push(in);
+        //唤醒消费者
+        pthread_cond_signal(&_c_cond);
+        pthread_mutex_unlock(&_lock);
     }
+
     void Pop(T*out)
     {
+        pthread_mutex_lock(&_lock);
+        if(IsEmpty())
+        {
+            pthread_cond_wait(&_c_cond,&_lock);
+        }
+        *out=_bq.front();
+        _bq.pop();
 
+        pthread_cond_signal(&_p_cond);
+        pthread_mutex_unlock(&_lock);
     }
     ~BlockQueue()
     {
-        pthread_mutex_destroy(_lock);
+        pthread_mutex_destroy(&_lock);
         pthread_cond_destroy(&_c_cond);
         pthread_cond_destroy(&_p_cond);
     }
--- 10_22/BlockQueue/main.cc
+++ 10_22/BlockQueue/main.cc
@@ -1,22 +1,47 @@
-#include"BlockQueue.hpp"
+#include "BlockQueue.hpp"
+#include <unistd.h>
+
+struct ThreadData
+{
+    BlockQueue<int> *bq;
+    std::string name;
+};
 
 void *consumer(void *args)
 {
-
+    ThreadData *td = static_cast<ThreadData *>(args);
+    int data = 0;
+    while (true)
+    {
+        sleep(1);
+        td->bq->Pop(&data);
+        std::cout << "消费者消费了一个数据：" << data << std::endl;
+    }
 }
 
-void* productor(void *args)
+void *productor(void *args)
 {
-
+    ThreadData *td = static_cast<ThreadData *>(args);
+    int data = 1;
+    while (true)
+    {
+        td->bq->Enqueue(data);
+        std::cout << "生产者生产了一个数据：" << data++ << std::endl;
+    }
 }
 
 int main()
 {
-    pthread_t c,p;
-    pthread_create(&c,nullptr,consumer,(void*)"消费者");
-    pthread_create(&p,nullptr,productor,(void*)"生产者");
+    BlockQueue<int> *bq = new BlockQueue<int>();
+    pthread_t c, p;
 
-    pthread_join(c,nullptr);
-    pthread_join(p,nullptr);
+    ThreadData ctd = {bq, "消费者"};
+    pthread_create(&c, nullptr, consumer, (void *)&ctd);
+
+    ThreadData ptd = {bq, "生产者"};
+    pthread_create(&p, nullptr, productor, (void *)&ptd);
+
+    pthread_join(c, nullptr);
+    pthread_join(p, nullptr);
     return 0;
 }
