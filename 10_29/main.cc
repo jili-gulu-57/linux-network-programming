f39f828154a2613a453533351e661d56945c4fcf
jili-gulu-57 <2263293720@qq.com>
2025-10-31 21:48:48 +0800

    线程池（2）

--- 10_29/main.cc
+++ 10_29/main.cc
@@ -1,1 +1,13 @@
-q
+#include"Logger.hpp"
+#include<unistd.h>
+
+int main()
+{
+    EnableConsoleLogStrategy();
+    // EnableFileLogStrategy();
+    LOG(LogLevel::DEBUG)<<"hello,world"<<1234;
+    LOG(LogLevel::WARNING)<<"hello,world"<<1234;
+    LOG(LogLevel::DEBUG)<<"hello,world"<<1234;
+    LOG(LogLevel::DEBUG)<<"hello,world"<<1234;
+    return 0;
+}
