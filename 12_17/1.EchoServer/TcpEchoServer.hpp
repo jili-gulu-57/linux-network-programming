0ec9c21d6b0836394ecbbc485626dee629508800
jili-gulu-57 <2263293720@qq.com>
2025-12-23 23:47:56 +0800

    TcpEchoServer完成

--- 12_17/1.EchoServer/TcpEchoServer.hpp
+++ 12_17/1.EchoServer/TcpEchoServer.hpp
@@ -5,6 +5,9 @@
 #include <cstring>
 #include <sys/socket.h>
 #include <arpa/inet.h>
+#include <unistd.h>
+#include <sys/wait.h>
+#include <pthread.h>
 #include "Mutex.hpp"
 #include "Logger.hpp"
 #include "Comm.hpp"
@@ -32,16 +35,16 @@ private:
                 std::string echo_string = "server echo: ";
                 echo_string += buffer;
 
-                //字符串回显到服务器端
-                LOG(LogLevel::INFO)<<"client say: "<<buffer;
-                
+                // 字符串回显到服务器端
+                LOG(LogLevel::INFO) << "client say: " << buffer;
+
                 // 将字符串回显到客户端
                 write(sockfd, echo_string.c_str(), echo_string.size());
             }
             // 用户退出：EOF
             else if (n == 0)
             {
-                LOG(LogLevel::INFO) << "client " << client.ToString() << "quit";
+                LOG(LogLevel::INFO) << "client " << client.ToString() << " quit";
                 break;
             }
             // 读取发生错误
@@ -79,7 +82,7 @@ public:
         // local.sin_family = AF_INET;
         // local.sin_addr.s_addr = htonl(INADDR_ANY);
 
-        InetAddr local(_port,"0.0.0.0");
+        InetAddr local(_port, "0.0.0.0");
 
         if (bind(_listensockfd, local.Addr(), local.Length()) != 0) // bind失败
         {
@@ -108,6 +111,31 @@ public:
         LOG(LogLevel::INFO) << "socket listen success" << _listensockfd;
     }
 
+    class ThreadData
+    {
+    public:
+        ThreadData(int sockfd, TcpEchoServer *self, const InetAddr &addr)
+            : _sockfd(sockfd), _self(self), _addr(addr)
+        {
+        }
+
+    public:
+        int _sockfd;
+        TcpEchoServer *_self;
+        InetAddr _addr;
+    };
+
+    static void *Routine(void *args)
+    {
+        ThreadData *td = static_cast<ThreadData *>(args);
+        pthread_detach(pthread_self());
+        td->_self->HandlerIO(td->_sockfd,td->_addr);
+
+        delete td;
+
+        return nullptr;
+    }
+
     void Start()
     {
         while (true) // 死循环，持续接收连接
@@ -126,9 +154,49 @@ public:
             // 封装客户端地址
             InetAddr clientaddr(peer);
             LOG(LogLevel::INFO) << "获取链接成功,socket: " << sockfd
-                                << "client addr: " << clientaddr.ToString();
+                                << " client addr: " << clientaddr.ToString();
 
-            HandlerIO(sockfd, clientaddr);
+            // version 3：多线程
+            pthread_t tid;
+            ThreadData *td = new ThreadData(sockfd, this, clientaddr); // this为当前TcpEchoServer指针
+            pthread_create(&tid, nullptr, Routine, (void *)td);
+
+            // version 1：单处理
+            //  HandlerIO(sockfd, clientaddr);
+
+            // version 2：多进程
+            // 创建子进程来同时处理多个
+            // pid_t id = fork();
+
+            // if (id > 0)
+            // {
+            //     // father
+            //     //父进程不进行对sockfd的连接，所以要关闭以免后续出错
+            //     close(sockfd);  //关闭对客户端sockfd的连接
+
+            //     pid_t rid = waitpid(id, nullptr, 0);
+            //     (void)rid;
+            // }
+            // else if (id == 0)
+            // {
+            //     // child
+            //     close(_listensockfd);
+
+            //     //创建孙子进程来完成对客户端的处理，这样父进程
+            //     if (fork() > 0) //该进程退出，父进程阻塞等待结束，可以接续接收新客户端的连接
+            //         exit(OK);
+
+            //     //孙子进程完成对客户端的IO处理，并且由于它的父进程退出，所以该孙子进程变成孤儿进程，由bash进程托管
+            //     HandlerIO(sockfd, clientaddr);
+            //     exit(OK);
+            // }
+            // else
+            // {
+            //     // id<0
+            //     // 资源不足
+            //     LOG(LogLevel::FATAL) << "资源不足，创建子进程失败";
+            //     exit(FORK_ERR);
+            // }
         }
     }
 
