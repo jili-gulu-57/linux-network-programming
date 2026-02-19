12936a9790480ce0ca5c24015e05fc4f18046a94
jili-gulu-57 <2263293720@qq.com>
2026-02-19 23:57:08 +0800

    epoll模型学习

--- 2_13/EpollEchpServer/EpollEchoServer.hpp
+++ 2_13/EpollEchpServer/EpollEchoServer.hpp
@@ -10,21 +10,33 @@
 class EpollServer
 {
 public:
-    EpollServer()
+    EpollServer(uint16_t port)
+        : _listensock(std::make_unique<TcpSocket>())
     {
+        _listensock->BuildListenSocketMethod(port);
+        _epfd = epoll_create(128); // 内核2.6.8之后参数被省略，但需传入一个正整数
+        if (_epfd < 0)
+        {
+            LOG(LogLevel::FATAL) << "epoll create fail!";
+            return;
+        }
+        LOG(LogLevel::INFO) << "listen sock is:" << _listensock->Sockfd() << "epoll fd is:" << _epfd;
+        struct epoll_event ev;
+        ev.data.fd = _listensock->Sockfd();
+        ev.events = EPOLLIN;
 
+        int n = epoll_ctl(_epfd, EPOLL_CTL_ADD, _listensock->Sockfd(), &ev);
     }
 
     void Start()
     {
-
     }
 
     ~EpollServer()
     {
-
     }
 
 private:
     std::unique_ptr<Socket> _listensock;
+    int _epfd; // epoll_create返回值
 };
